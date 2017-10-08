ee400cbe1fffdb72503168f5cab6bd3e9a751f98
Akarawin Satho <Akwin.winny@gmail.com>
2017-10-09 00:26:53 +0700

    add new Message and Create Message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
 	console.log('new user connected');
 
+	socket.emit('newMessage',{
+		from : 'john',
+		text: 'see ya ',
+		createAt: 112323
+	});
+
+	socket.on('createMessage',(message) => {
+		console.log('createMessage',message);
+	});
+
 	socket.on('disconnect',() => {
 		console.log('user was disconnect');
 	});
